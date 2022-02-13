b49ad0646161d2275fa6f84e4b5ac4ed2664ac6b
José Uilton Ferreira de Siqueira <programasuilton@gmail.com>
2022-02-12 21:46:04 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Github Explorer
+Um projeto para entender os conceitos básicos de React e Typescript, criado durante as aulas do Ignite de ReactJS.
+Nesse projeto configurado do zero consumimos a API do Github para listar os repositórios da rocketseat
---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+import { RepositoryItem } from "./RepositoryItem";
+import '../styles/repositories.scss';
+
+// https://api.github.com/orgs/rocketseat/repos
+
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+};
+
+export function RepositoryList() {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data));
+  }, []);
+
+ return (
+  <section className="repository-list">
+    <h1>Lista de repositórios</h1>
+
+    <ul>
+      { repositories.map(repository => <RepositoryItem key={repository.name} repository={repository} />) }
+    </ul>
+  </section>
+ );
+}
